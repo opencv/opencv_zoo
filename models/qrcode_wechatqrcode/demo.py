1ef77b47f000304d105abc90afac6a2d627352a4
fengyuentau <yuantao.feng@opencv.org.cn>
2021-11-04 11:10:45 +0800

    imporve visualize in demo

--- models/qrcode_wechatqrcode/demo.py
+++ models/qrcode_wechatqrcode/demo.py
@@ -30,9 +30,12 @@ parser.add_argument('--save', '-s', type=str2bool, default=False, help='Set true
 parser.add_argument('--vis', '-v', type=str2bool, default=True, help='Set true to open a window for result visualization. This flag is invalid when using camera.')
 args = parser.parse_args()
 
-def visualize(image, res, points, points_color=(0, 255, 0), text_color=(0, 255, 0)):
+def visualize(image, res, points, points_color=(0, 255, 0), text_color=(0, 255, 0), fps=None):
     output = image.copy()
     h, w, _ = output.shape
+
+    if fps is not None:
+        cv.putText(output, 'FPS: {:.2f}'.format(fps), (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, text_color)
 
     fontScale = 0.5
     fontSize = 1
@@ -41,10 +44,13 @@ def visualize(image, res, points, points_color=(0, 255, 0), text_color=(0, 255, 
         for _p in p:
             cv.circle(output, _p, 10, points_color, -1)
 
+        qrcode_center_x = int((p[0][0] + p[2][0]) / 2)
+        qrcode_center_y = int((p[0][1] + p[2][1]) / 2)
+
         text_size, baseline = cv.getTextSize(r, cv.FONT_HERSHEY_DUPLEX, fontScale, fontSize)
-        text_center_x = int((w - text_size[0]) / 2) if int((w - text_size[0]) / 2) >=0 else 0
-        text_center_y = int((h - text_size[1]) / 2)
-        cv.putText(output, '{}'.format(r), (text_center_x, text_center_y), cv.FONT_HERSHEY_DUPLEX, fontScale, text_color, fontSize)
+        text_x = qrcode_center_x - int(text_size[0] / 2)
+        text_y = qrcode_center_y - int(text_size[1] / 2)
+        cv.putText(output, '{}'.format(r), (text_x, text_y), cv.FONT_HERSHEY_DUPLEX, fontScale, text_color, fontSize)
 
     return output
 
@@ -91,12 +97,12 @@ if __name__ == '__main__':
 
             # Inference
             tm.start()
-            res, point = model.infer(frame)
+            res, points = model.infer(frame)
             tm.stop()
             fps = tm.getFPS()
 
             # Draw results on the input image
-            frame = visualize(frame, res, points)
+            frame = visualize(frame, res, points, fps=fps)
 
             # Visualize results in a new window
             cv.imshow('WeChatQRCode Demo', frame)
