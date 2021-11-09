2023061232492b0daaa81cbe780249707e6dc2c7
Yuantao Feng <yuantao.feng@outlook.com>
2021-11-09 11:00:02 +0800

    Decoupling metrics from benchmark to allow different kinds of forward process (#14)
    * create metrics for different types of behavior
    
    * workable impl calling utils.METRICS in benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -2,61 +2,30 @@ import os
 import argparse
 
 import yaml
-import tqdm
 import numpy as np
 import cv2 as cv
 
+# from ..models import MODELS
 from models import MODELS
+from utils import METRICS
 
 parser = argparse.ArgumentParser("Benchmarks for OpenCV Zoo.")
 parser.add_argument('--cfg', '-c', type=str,
                     help='Benchmarking on the given config.')
 args = parser.parse_args()
 
-class Timer:
-    def __init__(self, warmup=0, reduction='median'):
-        self._warmup = warmup
-        self._reduction = reduction
-        self._tm = cv.TickMeter()
-        self._time_record = []
-        self._calls = 0
+def build_from_cfg(cfg, registery, key='name'):
+    obj_name = cfg.pop(key)
+    obj = registery.get(obj_name)
+    return obj(**cfg)
 
-    def start(self):
-        self._tm.start()
-
-    def stop(self):
-        self._tm.stop()
-        self._calls += 1
-        self._time_record.append(self._tm.getTimeMilli())
-        self._tm.reset()
-
-    def reset(self):
-        self._time_record = []
-        self._calls = 0
-
-    def getResult(self):
-        if self._reduction == 'median':
-            return self._getMedian(self._time_record[self._warmup:])
-        elif self._reduction == 'gmean':
-            return self._getGMean(self._time_record[self._warmup:])
+def prepend_pythonpath(cfg):
+    for k, v in cfg.items():
+        if isinstance(v, dict):
+            prepend_pythonpath(v)
         else:
-            raise NotImplementedError()
-
-    def _getMedian(self, records):
-        ''' Return median time
-        '''
-        l = len(records)
-        mid = int(l / 2)
-        if l % 2 == 0:
-            return (records[mid] + records[mid - 1]) / 2
-        else:
-            return records[mid]
-
-    def _getGMean(self, records, drop_largest=3):
-        ''' Return geometric mean of time
-        '''
-        time_record_sorted = sorted(records, reverse=True)
-        return sum(records[drop_largest:]) / (self._calls - drop_largest)
+            if 'path' in k.lower():
+                cfg[k] = os.path.join(os.environ['PYTHONPATH'], v)
 
 class Data:
     def __init__(self, **kwargs):
@@ -105,56 +74,6 @@ class Data:
         else:
             return self._files[idx], image
 
-class Metric:
-    def __init__(self, **kwargs):
-        self._sizes = kwargs.pop('sizes', None)
-        self._warmup = kwargs.pop('warmup', 3)
-        self._repeat = kwargs.pop('repeat', 10)
-        assert self._warmup < self._repeat, 'The value of warmup must be smaller than the value of repeat.'
-        self._batch_size = kwargs.pop('batchSize', 1)
-        self._reduction = kwargs.pop('reduction', 'median')
-
-        self._timer = Timer(self._warmup, self._reduction)
-
-    def getReduction(self):
-        return self._reduction
-
-    def forward(self, model, *args, **kwargs):
-        img = args[0]
-        h, w, _ = img.shape
-        if not self._sizes:
-            self._sizes = [[w, h]]
-
-        results = dict()
-        self._timer.reset()
-        if len(args) == 1:
-            for size in self._sizes:
-                img_r = cv.resize(img, size)
-                try:
-                    model.setInputSize(size)
-                except:
-                    pass
-                # TODO: batched inference
-                # input_data = [img] * self._batch_size
-                input_data = img_r
-                for _ in range(self._repeat+self._warmup):
-                    self._timer.start()
-                    model.infer(input_data)
-                    self._timer.stop()
-                results[str(size)] = self._timer.getResult()
-        else:
-            # TODO: batched inference
-            # input_data = [args] * self._batch_size
-            bboxes = args[1]
-            for idx, bbox in enumerate(bboxes):
-                for _ in range(self._repeat+self._warmup):
-                    self._timer.start()
-                    model.infer(img, bbox)
-                    self._timer.stop()
-                results['bbox{}'.format(idx)] = self._timer.getResult()
-
-        return results
-
 class Benchmark:
     def __init__(self, **kwargs):
         self._data_dict = kwargs.pop('data', None)
@@ -162,7 +81,8 @@ class Benchmark:
         self._data = Data(**self._data_dict)
 
         self._metric_dict = kwargs.pop('metric', None)
-        self._metric = Metric(**self._metric_dict)
+        # self._metric = Metric(**self._metric_dict)
+        self._metric = build_from_cfg(self._metric_dict, registery=METRICS, key='type')
 
         backend_id = kwargs.pop('backend', 'default')
         available_backends = dict(
@@ -205,20 +125,6 @@ class Benchmark:
             for key, latency in results.items():
                 total_latency += latency
                 print('      {}, latency ({}): {:.4f} ms'.format(key, self._metric.getReduction(), latency))
-
-
-def build_from_cfg(cfg, registery):
-    obj_name = cfg.pop('name')
-    obj = registery.get(obj_name)
-    return obj(**cfg)
-
-def prepend_pythonpath(cfg):
-    for k, v in cfg.items():
-        if isinstance(v, dict):
-            prepend_pythonpath(v)
-        else:
-            if 'path' in k.lower():
-                cfg[k] = os.path.join(os.environ['PYTHONPATH'], v)
 
 if __name__ == '__main__':
     assert args.cfg.endswith('yaml'), 'Currently support configs of yaml format only.'
---
+++ benchmark/utils/metrics/detection.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+from .base_metric import BaseMetric
+from ..factory import METRICS
+
+@METRICS.register
+class Detection(BaseMetric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def forward(self, model, *args, **kwargs):
+        img = args[0]
+        if not self._sizes:
+            h, w, _ = img.shape
+            self._sizes.append([w, h])
+
+        results = dict()
+        self._timer.reset()
+        for size in self._sizes:
+            input_data = cv.resize(img, size)
+            try:
+                model.setInputSize(size)
+            except:
+                pass
+            for _ in range(self._warmup):
+                model.infer(input_data)
+            for _ in range(self._repeat):
+                self._timer.start()
+                model.infer(input_data)
+                self._timer.stop()
+            results[str(size)] = self._getResult()
+
+        return results
