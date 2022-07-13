6001f6099cc2f99041f6fb466841ff8c9c85ced7
张通 <11911611@mail.sustech.edu.cn>
2022-07-13 21:48:33 +0800

    add text detection metrics

--- models/text_detection_db/validation.py
+++ models/text_detection_db/validation.py
@@ -5,11 +5,16 @@
 # Third party copyrights are property of their respective owners.
 
 import argparse
+import glob
+import sys
+import time
 
 import numpy as np
 import cv2 as cv
 
 from db import DB
+from cal_rescall.script import cal_recall_precison_f1
+
 
 def str2bool(v):
     if v.lower() in ['on', 'yes', 'true', 'y', 't']:
@@ -34,6 +39,9 @@ except:
 parser = argparse.ArgumentParser(description='Real-time Scene Text Detection with Differentiable Binarization (https://arxiv.org/abs/1911.08947).')
 parser.add_argument('--input', '-i', type=str, help='Path to the input image. Omit for using default camera.')
 parser.add_argument('--model', '-m', type=str, default='text_detection_DB_TD500_resnet18_2021sep.onnx', help='Path to the model.')
+parser.add_argument('--gt_dir', type=str, default='icdar2015/test_gts', help='Path to the ground truth txt directory.')
+parser.add_argument('--out_dir', type=str, default='icdar2015/test_predicts', help='Path to the output txt directory.')
+parser.add_argument('--img_dir', type=str, default='icdar2015/test_images', help='Path to the test images directory.')
 parser.add_argument('--backend', '-b', type=int, default=backends[0], help=help_msg_backends.format(*backends))
 parser.add_argument('--target', '-t', type=int, default=targets[0], help=help_msg_targets.format(*targets))
 parser.add_argument('--width', type=int, default=736,
@@ -42,24 +50,27 @@ parser.add_argument('--height', type=int, default=736,
                     help='Preprocess input image by resizing to a specific height. It should be multiple by 32.')
 parser.add_argument('--binary_threshold', type=float, default=0.3, help='Threshold of the binary map.')
 parser.add_argument('--polygon_threshold', type=float, default=0.5, help='Threshold of polygons.')
+parser.add_argument('--iou_constraint', type=float, default=0.5, help='IOU constraint.')
+parser.add_argument('--area_precision_constraint', type=float, default=0.3, help='Area precision constraint.')
 parser.add_argument('--max_candidates', type=int, default=200, help='Max candidates of polygons.')
 parser.add_argument('--unclip_ratio', type=np.float64, default=2.0, help=' The unclip ratio of the detected text region, which determines the output size.')
 parser.add_argument('--save', '-s', type=str, default=False, help='Set true to save results. This flag is invalid when using camera.')
 parser.add_argument('--vis', '-v', type=str2bool, default=True, help='Set true to open a window for result visualization. This flag is invalid when using camera.')
 args = parser.parse_args()
 
-def visualize(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255), isClosed=True, thickness=2, fps=None):
-    output = image.copy()
 
-    if fps is not None:
-        cv.putText(output, 'FPS: {:.2f}'.format(fps), (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, text_color)
-
-    pts = np.array(results[0])
-    output = cv.polylines(output, pts, isClosed, box_color, thickness)
-
-    return output
 
 if __name__ == '__main__':
+    default_evaluation_params={
+        'IOU_CONSTRAINT': args.iou_constraint,
+        'AREA_PRECISION_CONSTRAINT': args.area_precision_constraint,
+        'GT_SAMPLE_NAME_2_ID': 'gt_img_([0-9]+).txt',
+        'DET_SAMPLE_NAME_2_ID': 'res_img_([0-9]+).txt',
+        'LTRB': False,  # LTRB:2points(left,top,right,bottom) or 4 points(x1,y1,x2,y2,x3,y3,x4,y4)
+        'CRLF': False,  # Lines are delimited by Windows CRLF format
+        'CONFIDENCES': True,  # Detections must include confidence value. AP will be calculated
+        'PER_SAMPLE_RESULTS':False  # Generate per sample results and produce data for visualization
+    }
     # Instantiate DB
     model = DB(modelPath=args.model,
                inputSize=[args.width, args.height],
@@ -70,55 +81,26 @@ if __name__ == '__main__':
                backendId=args.backend,
                targetId=args.target
     )
-
-    # If input is an image
-    if args.input is not None:
-        image = cv.imread(args.input)
+    files = glob.glob(args.img_dir+'/*', recursive=True)
+    start = time.time()
+    for file in files:
+        image = cv.imread(file)
         image = cv.resize(image, [args.width, args.height])
 
         # Inference
         results = model.infer(image)
-
-        # Print results
-        print('{} texts detected.'.format(len(results[0])))
+        img_name=file.split('/')[-1].split('.')[0]
+        text_file = args.out_dir+'res_' + img_name + '.txt'
+        result=''
         for idx, (bbox, score) in enumerate(zip(results[0], results[1])):
-            print('{}: {} {} {} {}, {:.2f}'.format(idx, bbox[0], bbox[1], bbox[2], bbox[3], score))
+            result+='{},{},{},{},{},{},{},{},{}\n'.format(bbox[0][0],bbox[0][1], bbox[1][0], bbox[1][1], bbox[2][0],bbox[2][1], bbox[3][0], bbox[3][1],score)
+        with open(text_file, 'w+') as fid:
+            fid.write(result)
+    end = time.time()
+    avg_time=(end-start)/len(files)
+    result_dict = cal_recall_precison_f1(args.gt_dir,args.out_dir,default_evaluation_params)
+    # Print results
+    result_dict['avg time']=avg_time
+    print(result_dict)
 
-        # Draw results on the input image
-        image = visualize(image, results)
-
-        # Save results if save is true
-        if args.save:
-            print('Results saved to result.jpg\n')
-            cv.imwrite('result.jpg', image)
-
-        # Visualize results in a new window
-        if args.vis:
-            cv.namedWindow(args.input, cv.WINDOW_AUTOSIZE)
-            cv.imshow(args.input, image)
-            cv.waitKey(0)
-    else: # Omit input to call default camera
-        deviceId = 0
-        cap = cv.VideoCapture(deviceId)
-
-        tm = cv.TickMeter()
-        while cv.waitKey(1) < 0:
-            hasFrame, frame = cap.read()
-            if not hasFrame:
-                print('No frames grabbed!')
-                break
-
-            frame = cv.resize(frame, [args.width, args.height])
-            # Inference
-            tm.start()
-            results = model.infer(frame) # results is a tuple
-            tm.stop()
-
-            # Draw results on the input image
-            frame = visualize(frame, results, fps=tm.getFPS())
-
-            # Visualize results in a new Window
-            cv.imshow('{} Demo'.format(model.name), frame)
-
-            tm.reset()
 
