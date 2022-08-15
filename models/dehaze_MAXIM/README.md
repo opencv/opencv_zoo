f8dd597a42e19ffcf2dca68f1947449c95fbfad2
yunongLiu1 <yunong_l@163.com>
2022-08-15 22:36:22 +0100

    Update demo.py maxim.py and README

--- models/dehaze_MAXIM/README.md
+++ models/dehaze_MAXIM/README.md
@@ -2,10 +2,9 @@
 
 # MAXIM: Multi-Axis MLP for Image Processing (CVPR 2022 Oral)
 
-fixed model input to 512*640, padding with  'reflect' mode
 
 Notes:
-- 
+- The official implementation of MAXIM can be found [here](https://github.com/google-research/maxim)
 - This ONNX model has fixed input shape (512*640), any image smaller than this will be padded with reflection mode, and any image larger than this will cause error.
 
 ## Demo
@@ -24,6 +23,11 @@ python demo.py --input /path/to/image
 ![sample dehazed image 1](./examples/output/dehazed_image1.png)
 ![sample image 2](./examples/input/image2.png)
 ![sample dehazed image 2](./examples/output/dehazed_image2.png)
+![sample image 3](./examples/input/image3.png)
+![sample dehazed image 3](./examples/output/dehazed_image3.png)
+![sample image 4](./examples/input/image4.png)
+![sample dehazed image 4](./examples/output/dehazed_image4.png)
+
 
 
 ## License
--- models/dehaze_MAXIM/maxim.py
+++ models/dehaze_MAXIM/maxim.py
@@ -8,24 +8,193 @@
 import numpy as np
 import onnxruntime as ort
 from PIL import Image
-import os
+
+def _is_image_file(filename):
+    """Check if it is a valid image file by extension."""
+    return any(
+        filename.endswith(extension)
+        for extension in ['jpeg', 'JPEG', 'jpg', 'png', 'JPG', 'PNG', 'gif'])
+
+
+def _convert_input_type_range(img):
+    """Convert the type and range of the input image.
+
+    It converts the input image to np.float32 type and range of [0, 1].
+    It is mainly used for pre-processing the input image in colorspace
+    conversion functions such as rgb2ycbcr and ycbcr2rgb.
+    Args:
+    img (ndarray): The input image. It accepts:
+        1. np.uint8 type with range [0, 255];
+        2. np.float32 type with range [0, 1].
+    Returns:
+        (ndarray): The converted image with type of np.float32 and range of
+            [0, 1].
+    """
+    img_type = img.dtype
+    img = img.astype(np.float32)
+    if img_type == np.float32:
+        pass
+    elif img_type == np.uint8:
+        img /= 255.
+    else:
+        raise TypeError('The img type should be np.float32 or np.uint8, '
+                        f'but got {img_type}')
+    return img
+
+
+def _convert_output_type_range(img, dst_type):
+    """Convert the type and range of the image according to dst_type.
+
+    It converts the image to desired type and range. If `dst_type` is np.uint8,
+    images will be converted to np.uint8 type with range [0, 255]. If
+    `dst_type` is np.float32, it converts the image to np.float32 type with
+    range [0, 1].
+    It is mainly used for post-processing images in colorspace conversion
+    functions such as rgb2ycbcr and ycbcr2rgb.
+    Args:
+    img (ndarray): The image to be converted with np.float32 type and
+        range [0, 255].
+    dst_type (np.uint8 | np.float32): If dst_type is np.uint8, it
+        converts the image to np.uint8 type with range [0, 255]. If
+        dst_type is np.float32, it converts the image to np.float32 type
+        with range [0, 1].
+    Returns:
+    (ndarray): The converted image with desired type and range.
+    """
+    if dst_type not in (np.uint8, np.float32):
+        raise TypeError('The dst_type should be np.float32 or np.uint8, '
+                        f'but got {dst_type}')
+    if dst_type == np.uint8:
+        img = img.round()
+    else:
+        img /= 255.
+
+    return img.astype(dst_type)
+
+
+def _rgb2ycbcr(img, y_only=False):
+    """Convert an RGB image to YCbCr image.
+
+    This function produces the same results as Matlab's `rgb2ycbcr` function.
+    It implements the ITU-R BT.601 conversion for standard-definition
+    television. See more details in
+    https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.601_conversion.
+    It differs from a similar function in cv2.cvtColor: `RGB <-> YCrCb`.
+    In OpenCV, it implements a JPEG conversion. See more details in
+    https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion.
+
+    Args:
+    img (ndarray): The input image. It accepts:
+        1. np.uint8 type with range [0, 255];
+        2. np.float32 type with range [0, 1].
+    y_only (bool): Whether to only return Y channel. Default: False.
+    Returns:
+    ndarray: The converted YCbCr image. The output image has the same type
+        and range as input image.
+    """
+    img_type = img.dtype
+    img = _convert_input_type_range(img)
+    if y_only:
+        out_img = np.dot(img, [65.481, 128.553, 24.966]) + 16.0
+    else:
+        out_img = np.matmul(img,
+                            [[65.481, -37.797, 112.0], [128.553, -74.203, -93.786],
+                             [24.966, 112.0, -18.214]]) + [16, 128, 128]
+    out_img = _convert_output_type_range(out_img, img_type)
+    return out_img
+
+
+def _to_y_channel(img):
+    """Change to Y channel of YCbCr.
+
+    Args:
+    img (ndarray): Images with range [0, 255].
+    Returns:
+    (ndarray): Images with range [0, 255] (float type) without round.
+    """
+    img = img.astype(np.float32) / 255.
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = _rgb2ycbcr(img, y_only=True)
+        img = img[..., None]
+    return img * 255.
+
+
+def _augment_image(image, times=8):
+    """Geometric augmentation."""
+    if times == 4:  # only rotate image
+        images = []
+        for k in range(0, 4):
+            images.append(np.rot90(image, k=k))
+        images = np.stack(images, axis=0)
+    elif times == 8:  # roate and flip image
+        images = []
+        for k in range(0, 4):
+            images.append(np.rot90(image, k=k))
+        image = np.fliplr(image)
+        for k in range(0, 4):
+            images.append(np.rot90(image, k=k))
+        images = np.stack(images, axis=0)
+    else:
+        raise Exception(f'Error times: {times}')
+    return images
+
+
+def _deaugment_image(images, times=8):
+    """Reverse the geometric augmentation."""
+
+    if times == 4:  # only rotate image
+        image = []
+        for k in range(0, 4):
+            image.append(np.rot90(images[k], k=4 - k))
+        image = np.stack(image, axis=0)
+        image = np.mean(image, axis=0)
+    elif times == 8:  # roate and flip image
+        image = []
+        for k in range(0, 4):
+            image.append(np.rot90(images[k], k=4 - k))
+        for k in range(0, 4):
+            image.append(np.fliplr(np.rot90(images[4 + k], k=4 - k)))
+        image = np.mean(image, axis=0)
+    else:
+        raise Exception(f'Error times: {times}')
+    return image
+
+
+def _save_img(img, pth):
+    """Save an image to disk.
+
+    Args:
+    img: np.ndarry, [height, width, channels], img will be clipped to [0, 1]
+        before saved to pth.
+    pth: string, path to save the image to.
+    """
+    Image.fromarray(np.array(
+        (np.clip(img, 0., 1.) * 255.).astype(np.uint8))).save(pth, 'PNG')
+
+
+def _convert_img_shape(image):
+    """Pad the image to 512*640."""
+    height, width = image.shape[0], image.shape[1]
+    padh = 512 - height
+    padw = 640 - width
+    image = np.pad(image, [(0, padh), (0, padw), (0, 0)], mode='reflect')
+    return image
+
 
 class MAXIM:
-    def __init__(self, modelPath, has_target,input_file,target_filename,geometric_ensemble,save_img,output_file ):
+    def __init__(self, modelPath, has_target, input_file, target_filename, geometric_ensemble, save_img, output_file):
         self._modelPath = modelPath
-        self._model = self._create_maxim_model()
+        self._create_maxim_model()
         self._has_target = has_target
         self._input_file = input_file
         self._target_filename = target_filename
         self._geometric_ensemble = geometric_ensemble
         self._save = save_img
-        self._output_file = output_file 
-
+        self._output_file = output_file
 
     @property
     def name(self):
         return self.__class__.__name__
-
 
     def _create_maxim_model(self):
         print("Loading model....")
@@ -34,29 +203,21 @@ class MAXIM:
         self.label_name = self.model.get_outputs()[-1].name
         print("Done!")
 
-    # def setBackend(self, backend_id):
-    #     self._backendId = backend_id
-    #     self._model.setPreferableBackend(self._backendId)
-
-    # def setTarget(self, target_id):
-    #     self._targetId = target_id
-    #     self._model.setPreferableTarget(self._targetId)
-
     def _preprocess(self):
 
-        if not self._is_image_file(self._input_file):
+        if not _is_image_file(self._input_file):
             raise ValueError('Input file is not an image file.')
 
         input_file = self._input_file
         input_img = np.asarray(Image.open(input_file).convert('RGB'),
-                            np.float32) / 255.
+                               np.float32) / 255.
         if self._has_target:
             target_file = self._target_filename
-            if not self._is_image_file(target_file):
+            if not _is_image_file(target_file):
                 raise ValueError('Target file is not an image file.')
-                
+
             self.target_img = np.asarray(Image.open(target_file).convert('RGB'),
-                                    np.float32) / 255.
+                                         np.float32) / 255.
 
         # Padding images to have even shapes
         height, width = input_img.shape[0], input_img.shape[1]
@@ -64,35 +225,31 @@ class MAXIM:
         if height > 512 or width > 640:
             raise ValueError('Input image is too large, max size is 512x640.')
 
-        input_img = self._convert_img_shape(input_img)
+        input_img = _convert_img_shape(input_img)
 
         if self._geometric_ensemble:
-            input_img = self._augment_image(input_img, args.ensemble_times)
+            input_img = _augment_image(input_img, self._ensemble_times)
         else:
             input_img = np.expand_dims(input_img, axis=0)
 
         return input_img, height, width
 
     def infer(self):
-        # assert image.shape[0] == self._inputSize[1], '{} (height of input image) != {} (preset height)'.format(image.shape[0], self._inputSize[1])
-        # assert image.shape[1] == self._inputSize[0], '{} (width of input image) != {} (preset width)'.format(image.shape[1], self._inputSize[0])
-
         # Preprocess
-        input_img,height, width = self._preprocess()
+        input_img, height, width = self._preprocess()
 
         # Forward
         preds = self.model.run([self.label_name], {self.input_name: input_img})[0]
 
         # Postprocess
-        results, psnr = self._postprocess(preds,height, width)
+        results, psnr = self._postprocess(preds, height, width)
 
         return results, psnr
 
     def _postprocess(self, preds, height, width):
-
-        # De-ensemble by averaging inferenced results.
+        # De-ensemble by averaging inferences results.
         if self._geometric_ensemble:
-            preds = self._deaugment_image(preds, args.ensemble_times)
+            preds = _deaugment_image(preds, self._ensemble_times)
         else:
             preds = np.array(preds[0], np.float32)
 
@@ -114,40 +271,11 @@ class MAXIM:
 
         # save files
         if self._save:
-            # os.makedirs(os.self._output_file, exist_ok=True)
-            self._save_img(preds, self._output_file)
+            _save_img(preds, self._output_file)
 
         return preds, psnr
 
-    
-    def _recover_tree(self,keys, values):
-        """Recovers a tree as a nested dict from flat names and values.
-
-        This function is useful to analyze checkpoints that are saved by our programs
-        without need to access the exact source code of the experiment. In particular,
-        it can be used to extract an reuse various subtrees of the scheckpoint, e.g.
-        subtree of parameters.
-        Args:
-        keys: a list of keys, where '/' is used as separator between nodes.
-        values: a list of leaf values.
-        Returns:
-        A nested tree-like dict.
-        """
-        tree = {}
-        sub_trees = self._collections.defaultdict(list)
-        for k, v in zip(keys, values):
-            if '/' not in k:
-                tree[k] = v
-            else:
-                k_left, k_right = k.split('/', 1)
-                sub_trees[k_left].append((k_right, v))
-        for k, kv_pairs in sub_trees.items():
-            k_subtree, v_subtree = zip(*kv_pairs)
-            tree[k] = self._recover_tree(k_subtree, v_subtree)
-        return tree
-
-
-    def _calculate_psnr(self,img1, img2, crop_border, test_y_channel=False):
+    def _calculate_psnr(self, img1, img2, crop_border, test_y_channel=False):
         """Calculate PSNR (Peak Signal-to-Noise Ratio).
 
         Ref: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
@@ -170,184 +298,10 @@ class MAXIM:
             img2 = img2[crop_border:-crop_border, crop_border:-crop_border, ...]
 
         if test_y_channel:
-            img1 = self._to_y_channel(img1)
-            img2 = self._to_y_channel(img2)
+            img1 = _to_y_channel(img1)
+            img2 = _to_y_channel(img2)
 
         mse = np.mean((img1 - img2) ** 2)
         if mse == 0:
             return float('inf')
         return 20. * np.log10(255. / np.sqrt(mse))
-
-
-    def _convert_input_type_range(self,img):
-        """Convert the type and range of the input image.
-
-        It converts the input image to np.float32 type and range of [0, 1].
-        It is mainly used for pre-processing the input image in colorspace
-        convertion functions such as rgb2ycbcr and ycbcr2rgb.
-        Args:
-        img (ndarray): The input image. It accepts:
-            1. np.uint8 type with range [0, 255];
-            2. np.float32 type with range [0, 1].
-        Returns:
-            (ndarray): The converted image with type of np.float32 and range of
-                [0, 1].
-        """
-        img_type = img.dtype
-        img = img.astype(np.float32)
-        if img_type == np.float32:
-            pass
-        elif img_type == np.uint8:
-            img /= 255.
-        else:
-            raise TypeError('The img type should be np.float32 or np.uint8, '
-                            f'but got {img_type}')
-        return img
-
-
-    def _convert_output_type_range(self,img, dst_type):
-        """Convert the type and range of the image according to dst_type.
-
-        It converts the image to desired type and range. If `dst_type` is np.uint8,
-        images will be converted to np.uint8 type with range [0, 255]. If
-        `dst_type` is np.float32, it converts the image to np.float32 type with
-        range [0, 1].
-        It is mainly used for post-processing images in colorspace convertion
-        functions such as rgb2ycbcr and ycbcr2rgb.
-        Args:
-        img (ndarray): The image to be converted with np.float32 type and
-            range [0, 255].
-        dst_type (np.uint8 | np.float32): If dst_type is np.uint8, it
-            converts the image to np.uint8 type with range [0, 255]. If
-            dst_type is np.float32, it converts the image to np.float32 type
-            with range [0, 1].
-        Returns:
-        (ndarray): The converted image with desired type and range.
-        """
-        if dst_type not in (np.uint8, np.float32):
-            raise TypeError('The dst_type should be np.float32 or np.uint8, '
-                            f'but got {dst_type}')
-        if dst_type == np.uint8:
-            img = img.round()
-        else:
-            img /= 255.
-
-        return img.astype(dst_type)
-
-
-    def _rgb2ycbcr(self,img, y_only=False):
-        """Convert a RGB image to YCbCr image.
-
-        This function produces the same results as Matlab's `rgb2ycbcr` function.
-        It implements the ITU-R BT.601 conversion for standard-definition
-        television. See more details in
-        https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.601_conversion.
-        It differs from a similar function in cv2.cvtColor: `RGB <-> YCrCb`.
-        In OpenCV, it implements a JPEG conversion. See more details in
-        https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion.
-
-        Args:
-        img (ndarray): The input image. It accepts:
-            1. np.uint8 type with range [0, 255];
-            2. np.float32 type with range [0, 1].
-        y_only (bool): Whether to only return Y channel. Default: False.
-        Returns:
-        ndarray: The converted YCbCr image. The output image has the same type
-            and range as input image.
-        """
-        img_type = img.dtype
-        img = self._convert_input_type_range(img)
-        if y_only:
-            out_img = np.dot(img, [65.481, 128.553, 24.966]) + 16.0
-        else:
-            out_img = np.matmul(img,
-                                [[65.481, -37.797, 112.0], [128.553, -74.203, -93.786],
-                                [24.966, 112.0, -18.214]]) + [16, 128, 128]
-        out_img = self._convert_output_type_range(out_img, img_type)
-        return out_img
-
-
-    def _to_y_channel(self,img):
-        """Change to Y channel of YCbCr.
-
-        Args:
-        img (ndarray): Images with range [0, 255].
-        Returns:
-        (ndarray): Images with range [0, 255] (float type) without round.
-        """
-        img = img.astype(np.float32) / 255.
-        if img.ndim == 3 and img.shape[2] == 3:
-            img = self._rgb2ycbcr(img, y_only=True)
-            img = img[..., None]
-        return img * 255.
-
-
-    def _augment_image(sef,image, times=8):
-        """Geometric augmentation."""
-        if times == 4:  # only rotate image
-            images = []
-            for k in range(0, 4):
-                images.append(np.rot90(image, k=k))
-            images = np.stack(images, axis=0)
-        elif times == 8:  # roate and flip image
-            images = []
-            for k in range(0, 4):
-                images.append(np.rot90(image, k=k))
-            image = np.fliplr(image)
-            for k in range(0, 4):
-                images.append(np.rot90(image, k=k))
-            images = np.stack(images, axis=0)
-        else:
-            raise Exception(f'Error times: {times}')
-        return images
-
-
-    def _deaugment_image(self,images, times=8):
-        """Reverse the geometric augmentation."""
-
-        if times == 4:  # only rotate image
-            image = []
-            for k in range(0, 4):
-                image.append(np.rot90(images[k], k=4 - k))
-            image = np.stack(image, axis=0)
-            image = np.mean(image, axis=0)
-        elif times == 8:  # roate and flip image
-            image = []
-            for k in range(0, 4):
-                image.append(np.rot90(images[k], k=4 - k))
-            for k in range(0, 4):
-                image.append(np.fliplr(np.rot90(images[4 + k], k=4 - k)))
-            image = np.mean(image, axis=0)
-        else:
-            raise Exception(f'Error times: {times}')
-        return image
-
-
-    def _is_image_file(self,filename):
-        """Check if it is an valid image file by extension."""
-        return any(
-            filename.endswith(extension)
-            for extension in ['jpeg', 'JPEG', 'jpg', 'png', 'JPG', 'PNG', 'gif'])
-
-
-    def _save_img(self,img, pth):
-        """Save an image to disk.
-
-        Args:
-        img: np.ndarry, [height, width, channels], img will be clipped to [0, 1]
-            before saved to pth.
-        pth: string, path to save the image to.
-        """
-        Image.fromarray(np.array(
-            (np.clip(img, 0., 1.) * 255.).astype(np.uint8))).save(pth, 'PNG')
-
-
-    def _convert_img_shape(self,image):
-        """Pad the image to 512*640."""
-        height, width = image.shape[0], image.shape[1]
-        padh = 512 - height
-        padw = 640 - width
-        image = np.pad(image, [(0, padh), (0, padw), (0, 0)], mode='reflect')
-        return image
-
-
