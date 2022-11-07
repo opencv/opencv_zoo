9896009d734d742a5582b389ec86a1ad79becf28
Sidd1609 <srisiddarth16541@gmail.com>
2022-11-07 10:50:40 +0530

    ObjectDetection YoloX
    Delete yolox_tiny.onnx
    
    Delete yolox_nano.onnx

---
+++ models/object_detection_yolox/YoloX.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+
+class YoloX():
+    def __init__(self, modelPath, confThreshold=0.35, nmsThreshold=0.5, objThreshold=0.5):
+        self.num_classes = 80
+        self.net = cv2.dnn.readNet(modelPath)
+        self.input_size = (640, 640)
+        self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float32).reshape(1, 1, 3)
+        self.std = np.array([0.229, 0.224, 0.225], dtype=np.float32).reshape(1, 1, 3)
+        self.strides = [8, 16, 32]
+        self.confThreshold = confThreshold
+        self.nmsThreshold = nmsThreshold
+        self.objThreshold = objThreshold
+
+    def preprocess(self, img):
+        padded_img = np.ones((self.input_size[0], self.input_size[1], 3)) * 114.0
+        ratio = min(self.input_size[0] / img.shape[0], self.input_size[1] / img.shape[1])
+        resized_img = cv2.resize(
+            img, (int(img.shape[1] * ratio), int(img.shape[0] * ratio)), interpolation=cv2.INTER_LINEAR
+        ).astype(np.float32)
+        padded_img[: int(img.shape[0] * ratio), : int(img.shape[1] * ratio)] = resized_img
+        image = padded_img
+
+        image = image.astype(np.float32)
+        image = image[:, :, ::-1]
+        return image, ratio
+
+    def infer(self, srcimg):
+        img, ratio = self.preprocess(srcimg)
+        blob = cv2.dnn.blobFromImage(img)
+        self.net.setInput(blob)
+        outs = self.net.forward(self.net.getUnconnectedOutLayersNames())
+        predictions = self.postprocess(outs[0], ratio)
+        return predictions
+
+    def postprocess(self, outputs, ratio):
+        grids = []
+        expanded_strides = []
+        hsizes = [self.input_size[0] // stride for stride in self.strides]
+        wsizes = [self.input_size[1] // stride for stride in self.strides]
+
+        for hsize, wsize, stride in zip(hsizes, wsizes, self.strides):
+            xv, yv = np.meshgrid(np.arange(hsize), np.arange(wsize))
+            grid = np.stack((xv, yv), 2).reshape(1, -1, 2)
+            grids.append(grid)
+            shape = grid.shape[:2]
+            expanded_strides.append(np.full((*shape, 1), stride))
+
+        grids = np.concatenate(grids, 1)
+        expanded_strides = np.concatenate(expanded_strides, 1)
+        outputs[..., :2] = (outputs[..., :2] + grids) * expanded_strides
+        outputs[..., 2:4] = np.exp(outputs[..., 2:4]) * expanded_strides
+
+        predictions = outputs[0]
+
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
+
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2] / 2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3] / 2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2] / 2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3] / 2.
+        boxes_xyxy /= ratio
+
+        final_dets = []
+        num_classes = scores.shape[1]
+
+        for cls_ind in range(num_classes):
+            cls_scores = scores[:, cls_ind]
+            valid_score_mask = cls_scores > self.confThreshold
+
+            if valid_score_mask.sum() == 0:
+                continue
+
+            else:
+                valid_scores = cls_scores[valid_score_mask]
+                valid_boxes = boxes_xyxy[valid_score_mask]
+
+                keep = []
+                x1 = valid_boxes[:, 0]
+                y1 = valid_boxes[:, 1]
+                x2 = valid_boxes[:, 2]
+                y2 = valid_boxes[:, 3]
+
+                areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+                order = valid_scores.argsort()[::-1]
+
+                while order.size > 0:
+                    i = order[0]
+                    keep.append(i)
+                    xx1 = np.maximum(x1[i], x1[order[1:]])
+                    yy1 = np.maximum(y1[i], y1[order[1:]])
+                    xx2 = np.minimum(x2[i], x2[order[1:]])
+                    yy2 = np.minimum(y2[i], y2[order[1:]])
+
+                    w = np.maximum(0.0, xx2 - xx1 + 1)
+                    h = np.maximum(0.0, yy2 - yy1 + 1)
+                    inter = w * h
+                    ovr = inter / (areas[i] + areas[order[1:]] - inter)
+                    inds = np.where(ovr <= self.nmsThreshold)[0]
+                    order = order[inds + 1]
+                    if len(keep) > 0:
+                        cls_inds = np.ones((len(keep), 1)) * cls_ind
+                        dets = np.concatenate([valid_boxes[keep], valid_scores[keep, None], cls_inds], 1)
+                        final_dets.append(dets)
+
+        res_dets = np.concatenate(final_dets, 0)
+
+        if len(final_dets) == 0:
+            res_dets = np.array([])
+
+        return res_dets
---
+++ models/object_detection_yolox/demo.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+import argparse
+import time
+from YoloX import YoloX
+
+backends = [cv2.dnn.DNN_BACKEND_OPENCV, cv2.dnn.DNN_BACKEND_CUDA]
+targets = [cv2.dnn.DNN_TARGET_CPU, cv2.dnn.DNN_TARGET_CUDA, cv2.dnn.DNN_TARGET_CUDA_FP16]
+help_msg_backends = "Choose one of the computation backends: {:d}: OpenCV implementation (default); {:d}: CUDA"
+help_msg_targets = "Chose one of the target computation devices: {:d}: CPU (default); {:d}: CUDA; {:d}: CUDA fp16"
+
+try:
+    backends += [cv2.dnn.DNN_BACKEND_TIMVX]
+    targets += [cv2.dnn.DNN_TARGET_NPU]
+    help_msg_backends += "; {:d}: TIMVX"
+    help_msg_targets += "; {:d}: NPU"
+except:
+    print('This version of OpenCV does not support TIM-VX and NPU. Visit https://gist.github.com/Sidd1609/5bb321c8733110ed613ec120c7c02e41 for more information.')
+
+classes = (    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+               'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+               'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+               'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+               'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+               'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+               'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+               'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+               'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+               'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+               'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+               'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+               'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock',
+               'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+            )
+
+def vis(dets, res_img):
+    if dets is not None:
+        final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
+        for i in range(len(final_boxes)):
+            box = final_boxes[i]
+            cls_id = int(final_cls_inds[i])
+            score = final_scores[i]
+            if score < args.confidence:
+                continue
+
+            x0 = int(box[0])
+            y0 = int(box[1])
+            x1 = int(box[2])
+            y1 = int(box[3])
+
+            text = '{}:{:.1f}%'.format(classes[cls_id], score * 100)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+            cv2.rectangle(res_img, (x0, y0), (x1, y1), (0, 0, 255), 2)
+            cv2.rectangle(res_img, (x0, y0 + 1), (x0 + txt_size[0] + 1, y0 + int(1.5 * txt_size[1])), (255, 255, 255), -1)
+            cv2.putText(res_img, text, (x0, y0 + txt_size[1]), font, 0.4, (0, 255, 0), thickness=1)
+
+    else:
+        print("No detections")
+
+    return res_img
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Nanodet inference using OpenCV an contribution by Sri Siddarth Chakaravarthy part of GSOC_2022')
+    parser.add_argument('--model', type=str, default='yolox_s.onnx', help="Path to the model")
+    parser.add_argument('--input_type', type=str, default='image', help="Input types: image or video")
+    parser.add_argument('--image_path', type=str, default='test2.jpg', help="Image path")
+    parser.add_argument('--confidence', default=0.5, type=float, help='Class confidence')
+    parser.add_argument('--nms', default=0.5, type=float, help='Enter nms IOU threshold')
+    parser.add_argument('--obj', default=0.5, type=float, help='Enter object threshold')
+    parser.add_argument('--save', '-s', type=str, default=False, help='Set true to save results. This option is invalid when using camera.')
+    args = parser.parse_args()
+
+    model_net = YoloX(modelPath= args.model, confThreshold=args.confidence, nmsThreshold=args.nms, objThreshold=args.obj)
+
+    if (args.input_type=="image"):
+        srcimg = cv2.imread(args.image_path)
+
+        image = srcimg
+        a = time.time()
+        preds = model_net.infer(srcimg)
+        b = time.time()
+        print('Inference_Time:'+str(b-a)+' secs')
+        srcimg = vis(preds, image)
+
+        cv2.namedWindow(args.image_path, cv2.WINDOW_AUTOSIZE)
+        cv2.imshow(args.image_path, srcimg)
+        cv2.waitKey(0)
+
+        if args.save:
+            print('Resutls saved to result.jpg\n')
+            cv2.imwrite('result.jpg', image)
+
+    else:
+        print("Press 1 to stop video capture")
+        cap = cv2.VideoCapture(0)
+        tm = cv2.TickMeter()
+        total_frames = 0
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        size = (frame_width, frame_height)
+
+        while cv2.waitKey(1) < 0:
+            hasFrame, frame = cap.read()
+            if not hasFrame:
+                print('No frames grabbed!')
+                break
+
+            frame = cv2.flip(frame, 1)
+            srcimg = frame
+
+            # Inference
+            tm.start()
+            preds = model_net.infer(srcimg)
+            tm.stop()
+
+            srcimg = vis(preds, srcimg)
+
+            total_frames += 1
+            fps=tm.getFPS()
+
+            if fps > 0:
+                fps_label = "FPS: %.2f" % fps
+                cv2.putText(srcimg, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+            cv2.imshow("output", srcimg)
+
+            if cv2.waitKey(1) < 0:
+                print("Stream terminated")
+                break
+
+            if(args.save):
+                result.write(frame)
+
+        print("Total frames: " + str(total_frames))
