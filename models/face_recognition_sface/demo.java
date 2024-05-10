e09a04ea2d62e5f922a4615af4c0eded12e616f0
yangbr <74187378+12341234-yangboran@users.noreply.github.com>
2024-05-10 14:57:42 +0800

    Update demo.java
    Remove redundant comments

--- models/face_recognition_sface/demo.java
+++ models/face_recognition_sface/demo.java
@@ -29,6 +29,7 @@ public class FaceRecognizer {
     private static double l2norm_similar_threshold = 1.128;
 
     // Your full path of yunet model
+    // <a href="https://docs.opencv.org/4.8.0/df/d20/classcv_1_1FaceDetectorYN.html">FaceDetectorYN</a>
     private static String faceDetectModelPath = "/face_detection_yunet_2023mar.onnx";
     // state faceDetector
     private static FaceDetectorYN faceDetector = null;
@@ -92,36 +93,13 @@ public class FaceRecognizer {
         faceDetector.setInputSize(imgB.size());
         faceDetector.detect(imgB, faceB);
 
-        /*
-        // 3.Draw face info in img
-        // <a href="https://docs.opencv.org/4.8.0/df/d20/classcv_1_1FaceDetectorYN.html">FaceDetectorYN</a>
-        // Draw a face frame in imgA
-        Imgproc.rectangle(imgA, new Rect((int) (faceA.get(0, 0)[0]), (int) (faceA.get(0, 1)[0]), (int) (faceA.get(0, 2)[0]), (int) (faceA.get(0, 3)[0])), new Scalar(0, 255, 0), 2);
-        // Draw eyes/nose/mouth points on imgB
-        Imgproc.circle(imgA, new Point(faceA.get(0, 4)[0], faceA.get(0, 5)[0]), 2, new Scalar(255, 0, 0), 2);
-        Imgproc.circle(imgA, new Point(faceA.get(0, 6)[0], faceA.get(0, 7)[0]), 2, new Scalar(0, 0, 255), 2);
-        Imgproc.circle(imgA, new Point(faceA.get(0, 8)[0], faceA.get(0, 9)[0]), 2, new Scalar(0, 255, 0), 2);
-        Imgproc.circle(imgA, new Point(faceA.get(0, 10)[0], faceA.get(0, 11)[0]), 2, new Scalar(255, 0, 255), 2);
-        Imgproc.circle(imgA, new Point(faceA.get(0, 12)[0], faceA.get(0, 13)[0]), 2, new Scalar(0, 255, 255), 2);
-        // Imgcodecs.imwrite("", imgA);
-
-        // Draw a face frame in imgB
-        Imgproc.rectangle(imgB, new Rect((int) (faceB.get(0, 0)[0]), (int) (faceB.get(0, 1)[0]), (int) (faceB.get(0, 2)[0]), (int) (faceB.get(0, 3)[0])), new Scalar(0, 255, 0), 2);
-        // Draw eyes/nose/mouth in imgB
-        Imgproc.circle(imgB, new Point(faceB.get(0, 4)[0], faceB.get(0, 5)[0]), 2, new Scalar(255, 0, 0), 2);
-        Imgproc.circle(imgB, new Point(faceB.get(0, 6)[0], faceB.get(0, 7)[0]), 2, new Scalar(0, 0, 255), 2);
-        Imgproc.circle(imgB, new Point(faceB.get(0, 8)[0], faceB.get(0, 9)[0]), 2, new Scalar(0, 255, 0), 2);
-        Imgproc.circle(imgB, new Point(faceB.get(0, 10)[0], faceB.get(0, 11)[0]), 2, new Scalar(255, 0, 255), 2);
-        Imgproc.circle(imgB, new Point(faceB.get(0, 12)[0], faceB.get(0, 13)[0]), 2, new Scalar(0, 255, 255), 2);
-        // Imgcodecs.imwrite("", imgB);*/
-
-        // 4.Aligning image to put face on the standard position
+        // 3.Aligning image to put face on the standard position
         Mat alignFaceA = new Mat();
         faceRecognizer.alignCrop(imgA, faceA.row(0), alignFaceA);
         Mat alignFaceB = new Mat();
         faceRecognizer.alignCrop(imgB, faceB.row(0), alignFaceB);
 
-        // 5.Extracting face feature from aligned image
+        // 4.Extracting face feature from aligned image
         Mat featureA = new Mat();
         faceRecognizer.feature(alignFaceA, featureA);
         featureA = featureA.clone();
@@ -129,7 +107,7 @@ public class FaceRecognizer {
         faceRecognizer.feature(alignFaceB, featureB);
         featureB = featureB.clone();
 
-        // 6.FaceRecogniz. Calculating the distance between two face features. If the condition is met, it returns true
+        // 5.FaceRecogniz. Calculating the distance between two face features. If the condition is met, it returns true
         // Get cosine similar
         double match1 = faceRecognizer.match(featureA, featureB, FaceRecognizerSF.FR_COSINE);
         // Get l2norm similar
