5eab9ea83a3ed29e29782dc2064988df13bc9043
zhang yang <3055481367@qq.com>
2025-05-26 22:41:10 +0800

    update model name in demo

--- models/image_segmentation_efficientsam/demo.py
+++ models/image_segmentation_efficientsam/demo.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description='EfficientSAM Demo')
 parser.add_argument('--input', '-i', type=str,
                     help='Set input path to a certain image.')
 parser.add_argument('--model', '-m', type=str, default='image_segmentation_efficientsam_ti_2025april.onnx',
-                    help='Set model path, defaults to image_segmentation_efficientsam_ti_2024april.onnx.')
+                    help='Set model path, defaults to image_segmentation_efficientsam_ti_2025april.onnx.')
 parser.add_argument('--backend_target', '-bt', type=int, default=0,
                     help='''Choose one of the backend-target pair to run this demo:
                         {:d}: (default) OpenCV implementation + CPU,
